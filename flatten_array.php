bad6e092e42094edca9171e60efef55f5131b034
Tung Vu <vutung.ct@gmail.com>
2015-09-17 21:12:58 +0700

    First commit for flatten array snippet code

---
+++ flatten_array.php
@@ -0,0 +1,19 @@
+<?php
+$a = array(1, 2, array(array(3, array(4, array(5)))), array(6, 7, 8), array(9));
+echo implode(' ', flattenArray($a));
+
+function flattenArray($arr) {
+	$outputArr = array();
+	while (!empty($arr)) {
+		$e = array_shift($arr);
+		if (!is_array($e)) {
+			$outputArr[] = $e;
+		} else {
+			foreach(array_reverse($e) as $se) {
+				array_unshift($arr, $se);
+			}
+		}
+	}
+	
+	return $outputArr;
+// }
